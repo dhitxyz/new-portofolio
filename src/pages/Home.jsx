16012b8f395efe7508ad6974a56e51053f012820
Kadek Panji <95892136+dhitxyz@users.noreply.github.com>
2025-04-24 21:09:34 +0800

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,52 +8,10 @@ import Project from "./home/Project";
 import Education from "./home/Education";
 import Contact from "./home/Contact";
 
-// Register ScrollTrigger plugin
-gsap.registerPlugin(ScrollTrigger);
-
 const Home = () => {
-    useEffect(() => {
-          // Transform tanpa fade
-          gsap.fromTo(
-            ".hero", 
-            {
-             scale: 1,
-            }, 
-            {
-              scale: 0.5,
-              scrollTrigger: {
-                trigger: ".hero",
-                scrub: true,
-              }
-            }
-          );
-        
-          // Sembunyikan .hero saat sudah scroll keluar dari viewport atas
-          ScrollTrigger.create({
-            trigger: ".hero",
-            start: "bottom top", // Saat .hero keluar dari atas viewport
-            onLeave: () => {
-              gsap.set(".hero", { visibility: "hidden" });
-            },
-            onEnterBack: () => {
-              gsap.set(".hero", { visibility: "visible" });
-            }
-          });
-        
-          // ScrollTrigger untuk panel lain
-          gsap.utils.toArray(".panel").forEach((panel, i) => {
-            ScrollTrigger.create({
-              trigger: panel,
-              start: "top bottom-=2",
-              end: () => "+=" + (window.innerHeight * 2 - 4),
-            });
-          });
-        }, []);
-        
-
     return (
         <main>
-            <section className="panel hero">
+            <section>
                 <Hero />
             </section>
 
