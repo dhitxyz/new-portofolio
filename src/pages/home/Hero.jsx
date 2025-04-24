12f85f0caa9746ddbaee87b5f4f9c6485ca31789
Kadek Panji <95892136+dhitxyz@users.noreply.github.com>
2025-04-24 21:13:44 +0800

    Update Hero.jsx

--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -4,10 +4,6 @@ const Hero = () => {
     return (
     <section>
         <div class="justify-center flex flex-col py-8 px-4 mx-auto min-h-screen text-center -mt-20 md:-mt-0 lg:py-16 lg:px-1 2">
-            <h6 className="text-slate-400 tracking-[0.6em] md:text-xl text-[12px] mb-4">
-                <span className="whitespace-nowrap">KADEK</span>{" "}
-                <span className="whitespace-nowrap">PANJI</span>
-            </h6>
             <h1 className="text-[80px] md:text-[180px] leading-none font-medium text-white stroke-text mb-4">
                 <span className="block text-slate-800 dark:text-white">ENJOY</span>
                 <span className="block text-slate-500">YOUR</span>
