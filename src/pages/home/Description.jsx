38c79c8d65ff9a97c235e824018fec7646d10820
Kadek Panji <95892136+dhitxyz@users.noreply.github.com>
2025-04-24 20:47:14 +0800

    Update Description.jsx

--- src/pages/home/Description.jsx
+++ src/pages/home/Description.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Description = () => {
     return (
         <section className="flex flex-col items-start pt-50 pb-30">
-            <h1 className="text-lg md:text-xl font-light tracking-wide text-gray-400 mb-2">
+            <h1 className="text-[15px] md:text-xl font-light tracking-wide text-gray-400 mb-2">
                 HELLO! I'm Panji <span className="wave-hand">👋</span>
             </h1>
             <h2 className="text-[27px] md:text-5xl font-bold leading-tight">
